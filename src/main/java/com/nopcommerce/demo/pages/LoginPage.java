231816909d481e381fcdc8bab70da150d293b12a
eightprimesep22 <eightprimesep22@gmail.com>
2022-12-10 17:43:49 +0000

    Today's Update Page-factory

--- src/main/java/com/nopcommerce/demo/pages/LoginPage.java
+++ src/main/java/com/nopcommerce/demo/pages/LoginPage.java
@@ -3,18 +3,37 @@ package com.nopcommerce.demo.pages;
 import com.aventstack.extentreports.Status;
 import com.nopcommerce.demo.customlisteners.CustomListeners;
 import com.nopcommerce.demo.utility.Utility;
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Reporter;
 
 /**
  * Created by Jay Vaghani
  */
 public class LoginPage extends Utility {
 
-    By welcomeText = By.xpath("//h1[contains(text(),'Welcome, Please Sign In!')]");
+    @CacheLookup
+    @FindBy(xpath = "//h1[contains(text(),'Welcome, Please Sign In!')]")
+    WebElement welcomeText;
+    @CacheLookup
+    @FindBy(id = "Email")
+    WebElement emailField;
+
+    @FindBy(name = "Password")
+    WebElement passwordField;
+
+    @FindBy(xpath = "//button[contains(text(),'Log in')]")
+    WebElement loginButton;
+
+    @FindBy(xpath = "//div[@class='message-error validation-summary-errors']")
+    WebElement errorMessage;
+
+    /*By welcomeText = By.xpath("//h1[contains(text(),'Welcome, Please Sign In!')]");
     By emailField = By.id("Email");
     By passwordField = By.name("Password");
     By loginButton = By.xpath("//button[contains(text(),'Log in')]");
-    By errorMessage = By.xpath("//div[@class='message-error validation-summary-errors']");
+    By errorMessage = By.xpath("//div[@class='message-error validation-summary-errors']");*/
 
     public String getWelcomeText(){
         String message = getTextFromElement(welcomeText);
@@ -22,6 +41,7 @@ public class LoginPage extends Utility {
     }
 
     public void enterEmailId(String email){
+        Reporter.log("");
         sendTextToElement(emailField, email);
         CustomListeners.test.log(Status.PASS,"Enter EmailId " + email);
     }
--- src/test/java/com/nopcommerce/demo/testsuite/LoginPageTest.java
+++ src/test/java/com/nopcommerce/demo/testsuite/LoginPageTest.java
@@ -5,6 +5,7 @@ import com.nopcommerce.demo.pages.HomePage;
 import com.nopcommerce.demo.pages.LoginPage;
 import com.nopcommerce.demo.testbase.BaseTest;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -14,13 +15,19 @@ import org.testng.annotations.Test;
 @Listeners(CustomListeners.class)
 public class LoginPageTest extends BaseTest {
 
-    HomePage homePage = new HomePage();
-    LoginPage loginPage = new LoginPage();
+    HomePage homePage;
+    LoginPage loginPage;
+
+    @BeforeMethod
+    public void inIt() {
+        homePage = new HomePage();
+        loginPage = new LoginPage();
+    }
 
     @Test
     public void verifyUserShouldNavigateToLoginPage() {
         homePage.clickOnLoginLink();
-        String expectedMessage = "Welcome, Please Sign In!";
+        String expectedMessage = "Welcome, Please Sign In";
         String actualMessage = loginPage.getWelcomeText();
         Assert.assertEquals(expectedMessage, actualMessage, "Login page not displayed");
     }
@@ -34,6 +41,6 @@ public class LoginPageTest extends BaseTest {
         String expectedErrorMessage = "Login was unsuccessful. Please correct the errors and try again.\n"
                 + "No customer account found";
         String actualErrorMessage = loginPage.getErrorMessage();
-        Assert.assertEquals( expectedErrorMessage, actualErrorMessage,"Error message not displayed");
+        Assert.assertEquals(expectedErrorMessage, actualErrorMessage, "Error message not displayed");
     }
 }
